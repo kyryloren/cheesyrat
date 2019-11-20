84dde97e49fe7bd62aacdeb5cac4c5e55e3097d6
kyryloren <48699163+kyryloren@users.noreply.github.com>
2019-11-20 14:18:06 -0800

    Add generate menu

--- cheesyrat.py
+++ cheesyrat.py
@@ -19,17 +19,18 @@ def complete_menu():
     answ=True
     while answ:
         ans = ""
-        ans = cheesyrat_lib.input_func(colors.GREEN + colors.BOLD + ' ┌─[' + colors.RED + 'ChubbyBunny' + colors.GREEN + colors.BOLD + ']--[' + colors.RED + colors.BOLD + '-' + colors.GREEN + colors.BOLD + ']-[' + colors.YELLOW + 'menu' + colors.GREEN + colors.BOLD + ']:\n' + ' └─────► ' + colors.END)
+        ans = cheesyrat_lib.input_func(colors.GREEN + colors.BOLD + ' ┌─[' + colors.RED + 'cheesyrat' + colors.GREEN + colors.BOLD + ']--[' + colors.RED + colors.BOLD + '-' + colors.GREEN + colors.BOLD + ']-[' + colors.YELLOW + 'menu' + colors.GREEN + colors.BOLD + ']:\n' + ' └─────► ' + colors.END)
 
         if ans.lower() == "help":
             cheesyrat_lib.main_menu_help()
         elif ans == "?":
             cheesyrat_lib.main_menu_help()
         elif ans.lower() == "generate":
-            print("load generator")
+            generate_menu()
         elif ans.lower() == "listen":
             print("load listener")
         elif ans.lower() == "credits":
+            cheesyrat_lib.clear()
             cheesyrat_lib.credits()
             back = input(colors.GREEN + colors.BOLD + '\n\nPress [ENTER] to return to menu' + colors.END)
             cheesyrat_lib.clear()
@@ -44,6 +45,25 @@ def complete_menu():
             cheesyrat_lib.exit_function()
         else:
             cheesyrat_lib.warning_message("Invalid command.")
+
+def generate_menu():
+    while True:
+        cmd = cheesyrat_lib.input_func(colors.GREEN + colors.BOLD + ' ┌─[' + colors.RED + 'cheesyrat' + colors.GREEN + colors.BOLD + ']--[' + colors.RED + colors.BOLD + '-' + colors.GREEN + colors.BOLD + ']-[' + colors.YELLOW + 'main/generate' + colors.GREEN + colors.BOLD + ']:\n' + ' └─────► ' + colors.END)
+        if cmd.lower() == "help":
+            cheesyrat_lib.generate_menu_help()
+        elif cmd.lower() == "?":
+            cheesyrat_lib.generate_menu_help()
+        elif cmd.lower() == "clear":
+            cheesyrat_lib.clear()
+        elif cmd.lower() == "back":
+            break
+        elif cmd.lower() == "exit":
+            cheesyrat_lib.exit_function()
+        elif cmd.lower() == "quit":
+            cheesyrat_lib.exit_function()
+        else:
+            cheesyrat_lib.warning_message("Invalid command.")
+
 
 if __name__ == "__main__":
     complete_menu()
